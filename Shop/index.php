bad6c196fb5994944b3e91125336ad4c9640bb31
Bahadori Imani Group <121450429+GroupBahadoriImani@users.noreply.github.com>
2023-01-26 16:11:00 +0330

    Deleting Record

--- Shop/index.php
+++ Shop/index.php
@@ -2,6 +2,17 @@
 
 require_once 'dbconfig.php';
 
+if (isset($_REQUEST['del'])) {
+    $productId = intval($_GET['del']);
+
+    $sql = "DELETE FROM product WHERE Id =:id";
+    $query = $conn->prepare($sql);
+    $query->bindParam(':id', $productId, PDO::PARAM_STR);
+    $query->execute();
+    echo "<script>alert('حذف با موفقیت انجام شد')</script>";
+    echo "<script>window.lcation.href='index.php'</script>";
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -40,7 +51,7 @@ require_once 'dbconfig.php';
                         <tbody>
 
                             <?php
-                            $sql = "SELECT Name, Color, Size, Material, Price, Picture From product";
+                            $sql = "SELECT Id, Name, Color, Size, Material, Price, Picture From product";
                             $query = $conn->prepare($sql);
                             $query->execute();
                             $results = $query->fetchAll(PDO::FETCH_OBJ);
@@ -54,30 +65,36 @@ require_once 'dbconfig.php';
 
                                     <tr>
                                         <td>
-                                            <?php echo $id ?>
+                                            <?php echo htmlentities($id) ?>
                                         </td>
                                         <td>
-                                            <?php echo $result->Name ?>
+                                            <?php echo htmlentities($result->Name) ?>
                                         </td>
                                         <td>
-                                            <?php echo $result->Color ?>
+                                            <?php echo htmlentities($result->Color) ?>
                                         </td>
                                         <td>
-                                            <?php echo $result->Size ?>
+                                            <?php echo htmlentities($result->Size) ?>
                                         </td>
                                         <td>
-                                            <?php echo $result->Material ?>
+                                            <?php echo htmlentities($result->Material) ?>
                                         </td>
                                         <td>
-                                            <?php echo $result->Price ?>
+                                            <?php echo htmlentities($result->Price) ?>
                                         </td>
                                         <td>
-                                            <?php echo $result->Picture ?>
+                                            <?php echo htmlentities($result->Picture) ?>
                                         </td>
 
-                                        <td><a href="#"><button class="btn btn-warning"><span class="glyphicon glyphicon-pencil"></span></button></a></td>
+                                        <td><a href="update.php?id=<?php echo htmlentities($result->Id) ?>">
+                                                <button class="btn btn-warning">
+                                                    <span class="glyphicon glyphicon-pencil"></span>
+                                                </button></a></td>
 
-                                        <td><a href="#"><button class="btn btn-danger" onClick="return confirm('آیا حذف انجام شود');"><span class="glyphicon glyphicon-trash"></span></button></a></td>
+                                        <td><a href="index.php?del=<?php echo htmlentities($result->Id) ?>">
+                                                <button class="btn btn-danger" onClick="return confirm('آیا حذف انجام شود');">
+                                                    <span class="glyphicon glyphicon-trash"></span>
+                                                </button></a></td>
                                     </tr>
 
                             <?php
