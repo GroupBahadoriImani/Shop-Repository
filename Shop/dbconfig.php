cfaa8a80c92bb43792d0d11c8751b619f73f2f2d
Bahadori Imani Group <121450429+GroupBahadoriImani@users.noreply.github.com>
2023-01-26 14:12:42 +0330

    Deleting Database folder and create dbconfig for connecting to database

---
+++ Shop/dbconfig.php
@@ -0,0 +1,14 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'shopdb');
+
+try {
+    $conn = new PDO('mysql:host='. DB_HOST .';dbname=' . DB_NAME, DB_USER, DB_PASS);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected Successfully";
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
