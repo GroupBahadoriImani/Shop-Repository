8aad10d7788dbcc24a3ea5edd12dd5bbb1817d2a
Bahadori Imani Group <121450429+GroupBahadoriImani@users.noreply.github.com>
2023-01-26 17:03:41 +0330

    adding little changes

--- Shop/update.php
+++ Shop/update.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>PHP CRUD</title>
+    <title>فروشگاه لباس</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
