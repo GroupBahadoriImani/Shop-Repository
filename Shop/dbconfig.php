5d604e2005c9aa18b2c0a37420f04832e1f60853
Bahadori Imani Group <121450429+GroupBahadoriImani@users.noreply.github.com>
2023-01-26 15:18:48 +0330

    adding template and get information from database and show infos in index.php

--- Shop/dbconfig.php
+++ Shop/dbconfig.php
@@ -7,6 +7,7 @@ define('DB_NAME', 'shopdb');
 
 try {
     $conn = new PDO('mysql:host='. DB_HOST .';dbname=' . DB_NAME, DB_USER, DB_PASS);
+    $conn->exec('SET NAME utf8');
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected Successfully";
 } catch (PDOException $e) {
